9f501488b19f1a4ebaef04999f2ff8e81c7e0685
weevil <abhinavparagmishra23@gmail.com>
2021-04-10 18:36:18 +0530

    Add information page

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.use(express.json());
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-  res.sendFile( path.join(__dirname,"./index.html"));
+  res.sendFile( path.join(__dirname,"/index.html"));
+})
+
+app.get("/info", (req, res) => {
+  res.sendFile( path.join(__dirname,"/info.html"));
 })
 
 app.listen(3000)
